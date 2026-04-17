806b0fcbcd2956ea6690a0e628cef309fb36aa3e
maaurissio <maur.gajardo@duocuc.cl>
2026-04-16 20:14:20 -0400

    Nuevo MS-Users

---
+++ src/test/java/cl/duoc/cordillera/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package cl.duoc.cordillera;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
